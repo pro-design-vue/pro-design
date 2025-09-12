fac952031b42b0502c19bebdcd3ea36184259a12
xh-shen <9509394@qq.com>
2025-09-12 10:45:57 +0800

    fix: del antv App comp ssr error

--- docs/.vitepress/theme/App.vue
+++ docs/.vitepress/theme/App.vue
@@ -2,18 +2,18 @@
  * @Author: shen
  * @Date: 2025-09-06 11:17:01
  * @LastEditors: shen
- * @LastEditTime: 2025-09-08 09:40:00
+ * @LastEditTime: 2025-09-12 10:44:47
  * @Description:
 -->
 <script setup lang="ts">
-import { ProConfigProvider } from 'pro-design-vue'
+import { ConfigProvider } from 'ant-design-vue'
 import DefaultTheme from 'vitepress/theme'
 import antdDefaultLocale from 'ant-design-vue/es/locale/zh_CN'
 const { Layout } = DefaultTheme
 </script>
 
 <template>
-  <ProConfigProvider :locale="antdDefaultLocale">
+  <ConfigProvider :locale="antdDefaultLocale">
     <Layout />
-  </ProConfigProvider>
+  </ConfigProvider>
 </template>
