a5ea5a1ecd60c0286d7d1fa8ce0060ff2e6292c5
xh-shen <9509394@qq.com>
2025-10-10 15:48:48 +0800

    feat: update locale import path

--- packages/components/config-provider/src/config-provider.vue
+++ packages/components/config-provider/src/config-provider.vue
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2025-05-22 09:08:36
  * @LastEditors: shen
- * @LastEditTime: 2025-10-10 09:10:46
+ * @LastEditTime: 2025-10-10 15:05:21
  * @Description:
 -->
 <script setup lang="ts">
@@ -13,7 +13,7 @@ import { DEFAULT_LOCALE } from '@pro-design-vue/constants'
 import { zhCNIntl } from './intl'
 import dayjs from 'dayjs'
 import ConfigProviderContainer from './config-provider-container.vue'
-import antdDefaultLocale from 'ant-design-vue/es/locale/zh_CN'
+import antdDefaultLocale from 'ant-design-vue/es/locale/zh_CN.js'
 
 interface Props extends ProConfigProviderProps {}
 
@@ -33,7 +33,7 @@ const {
 } = defineProps<Props>()
 // 默认国际化为zh-cn
 if (locale.locale === DEFAULT_LOCALE.toLocaleLowerCase()) {
-  import('dayjs/locale/zh-cn').then((value) => {
+  import('dayjs/locale/zh-cn.js').then((value) => {
     dayjs.locale(value)
   })
 }
